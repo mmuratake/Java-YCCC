57667ad2c7b34d755a90b43a3789276211b6456f
sMamiMuratake <sMamiMuratake@C218-1761.yccc.edu>
2018-01-24 19:16:21 -0500

    Wrote some notes.

--- src/Class.java
+++ src/Class.java
@@ -1,5 +1,24 @@
 
 public class Class {
 	
+	public void testMe()
+	{
+		// Just to remind you what the heck you need to do with Git.
+		
+		// Save often, save often.
+		
+		// Drag from Unstaged Changes to Staged Changes, often, often. Otherwise you lose it.
+		
+		// Clone creates it locally.
+		
+		// Commit puts work into local repo.
+		
+		// Push pushes to cloud (on GitHub).
+		
+		// Pull takes from Repo.
+		
+		// Work on different computers, or work in a team, always pull.
+	}
+	
 
 }
