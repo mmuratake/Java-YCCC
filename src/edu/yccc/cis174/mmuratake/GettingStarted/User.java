438f222a44672544cc1efc95bada0aa2e1123f2e
sMamiMuratake <sMamiMuratake@C218-1761.yccc.edu>
2018-01-24 19:45:27 -0500

    I wrote a Class trying to make a user.

---
+++ src/edu/yccc/cis174/mmuratake/GettingStarted/User.java
@@ -0,0 +1,28 @@
+package edu.yccc.cis174.mmuratake.GettingStarted;
+
+public class User {
+	
+	public String userName;
+	public String userAge;
+	public String userGender;
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getUserAge() {
+		return userAge;
+	}
+	public void setUserAge(String userAge) {
+		this.userAge = userAge;
+	}
+	public String getUserGender() {
+		return userGender;
+	}
+	public void setUserGender(String userGender) {
+		this.userGender = userGender;
+	}
+	
+
+}
