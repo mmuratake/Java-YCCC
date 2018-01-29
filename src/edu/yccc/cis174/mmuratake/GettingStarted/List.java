fe9ce6cdd416295652c9857fad9494acaeb9d488
smamimuratake <smamimuratake@C218-1761.yccc.edu>
2018-01-29 17:13:22 -0500

    Added comments to code.

--- src/edu/yccc/cis174/mmuratake/GettingStarted/List.java
+++ src/edu/yccc/cis174/mmuratake/GettingStarted/List.java
@@ -1,5 +1,16 @@
 package edu.yccc.cis174.mmuratake.GettingStarted;
 
+/**
+ * @author smamimuratake
+ * January 29 2018
+ * Assignment #1: Getting Started
+ * 
+ * Creating a list.
+ * 
+ */
+
 public class List {
+	
+	
 
 }
