8b34fb5edb7ff3fc662455e122870631ce10075f
sMamiMuratake <sMamiMuratake@C218-1761.yccc.edu>
2018-01-31 14:10:03 -0500

    Mostly finished up the code. Added comments. Fixed details.

--- src/edu/yccc/cis174/mmuratake/GettingStarted/CreateUsers.java
+++ src/edu/yccc/cis174/mmuratake/GettingStarted/CreateUsers.java
@@ -38,11 +38,11 @@ public class CreateUsers {
 		u3.setUserAge(15);
 		u3.setUserGender("Gender Fluid");
 		
-		System.out.println("User #1");
+		System.out.println("User A");
 		System.out.println(u.getUserName() + ", " + u.getUserAge() + " years old; " + u.getUserGender());
-		System.out.println("User #2");
+		System.out.println("User B");
 		System.out.println(u2.getUserName() + ", " + u2.getUserAge() + " years old; " + u2.getUserGender());
-		System.out.println("User #3");
+		System.out.println("User C");
 		System.out.println(u3.getUserName() + ", " + u3.getUserAge() + " years old; " + u3.getUserGender());
 		
 		// Add users to the list (and take one out).
@@ -51,28 +51,32 @@ public class CreateUsers {
 		users.add(u3);
 		users.remove(u2);
 		
-		System.out.println(users);
-		// Even though you can't tell anything about the attributes of the users, you can tell that there are only two in the list.
-		
-		// If-Else statements about the list of users.
-		if(users.contains(u))
-		{
-			System.out.println("There is a male in here!");
-		}
-		else
-		{
-			System.out.println("There is a potential that all users are female.");
-		}
-		if(!users.contains(u2) && users.contains(u))
-		{
-			System.out.println("There is a potential that there are no females here!");
-		}
 		
 		// A for loop with the list of users.
 		for(int x = 0; x < users.size(); x++)
 		{
+			int userNumber = x+1;
+			System.out.println();
+			System.out.println("Hi, I'm User #" + userNumber + " in this user list.");
 			System.out.println(users.get(x));
+			// Even though you can't tell anything about the attributes of the users, you can tell that there are only two in the list.
 		}
+				
+				
+		// If-Else statements about the list of users.
+		if(users.contains(u))
+		{
+			System.out.println("\r\n>There is a male user in this list!");
+		}
+		else
+		{
+			System.out.println("\r\n>There is a potential that all users are female.");
+		}
+		if(!users.contains(u2) && users.contains(u))
+		{
+			System.out.println(">There is a potential that there are no females in this list!\r\n");
+		}
+	
 	}
 	
 	public static void main(String[] args)
@@ -80,6 +84,5 @@ public class CreateUsers {
 		CreateUsers c = new CreateUsers();
 		c.messWithUserList();
 	}
-		
 
 }
