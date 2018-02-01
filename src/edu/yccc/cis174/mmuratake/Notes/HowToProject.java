851c09bed04e1d15fbdff0314aa1919679957958
sMamiMuratake <sMamiMuratake@C218-1761.yccc.edu>
2018-01-31 19:40:32 -0500

    How to project project.

---
+++ src/edu/yccc/cis174/mmuratake/Notes/HowToProject.java
@@ -0,0 +1,22 @@
+package edu.yccc.cis174.mmuratake.Notes;
+
+public class HowToProject {
+	
+	//How to project project.
+	// # 1.
+	
+	/**
+	 *EXAM
+	 *
+	 * Question Object 
+	 * 	List<answer> answers
+	 * 	Readable Questions
+	 * 	Boolean = userAnswer
+	 * 
+	 * 
+	 * Answer
+	 * 	-isCorrect
+	 * 	-readableOption
+	 */
+
+}
