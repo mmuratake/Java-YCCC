141daac20c5468d910950c022006b76b5d5195ac
kanamamimuratake <kanamamimuratake@192.168.0.5>
2018-02-11 17:08:27 -0500

    Trying to make a question object.

--- src/edu/yccc/cis174/mmuratake/Project1/Questions.java
+++ src/edu/yccc/cis174/mmuratake/Project1/Questions.java
@@ -2,6 +2,24 @@ package edu.yccc.cis174.mmuratake.Project1;
 
 public class Questions {
 	
+	String question;
+	String answer;
 	
-
+	public String getQuestion() 
+	{
+		return question;
+	}
+	public void setQuestion(String question) 
+	{
+		this.question = question;
+	}
+	public String getAnswer() 
+	{
+		return answer;
+	}
+	public void setAnswer(String answer) 
+	{
+		this.answer = answer;
+	}
+	
 }
