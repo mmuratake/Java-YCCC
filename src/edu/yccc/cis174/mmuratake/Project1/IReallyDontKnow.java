ab9fe90ba731d0045e7f8e863e8102bf18866c67
kanamamimuratake <kanamamimuratake@192.168.0.5>
2018-02-12 17:42:01 -0500

    Writing out questions.

--- src/edu/yccc/cis174/mmuratake/Project1/IReallyDontKnow.java
+++ src/edu/yccc/cis174/mmuratake/Project1/IReallyDontKnow.java
@@ -11,6 +11,29 @@ public class IReallyDontKnow {
 		q.setChoiceC("A musical instrument");
 		q.setAnswer("B");
 		
+		Questions q2 = new Questions();
+		q2.setQuestion("How do you write a single-line comment in Java?");
+		q2.setAnswer("//");
+		
+		Questions q3 = new Questions();
+		q3.setQuestion("How do you write a multi-line comment in Java?");
+		q3.setAnswer("/**");
+		
+		Questions q4 = new Questions();
+		q4.setQuestion("What is the symbol for \"and\" in Java?");
+		q4.setChoiceA("&");
+		q4.setChoiceB("&&");
+		q4.setChoiceC("||");
+		q4.setAnswer("B");
+		
+		Questions q5 = new Questions();
+		Questions q6 = new Questions();
+		Questions q7 = new Questions();
+		Questions q8 = new Questions();
+		Questions q9 = new Questions();
+		Questions q10 = new Questions();
+
+		
 	}
 	
 }
