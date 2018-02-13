558f82952f17fe85c6d02d1d002a9d001a93ff2e
kanamamimuratake <kanamamimuratake@192.168.0.5>
2018-02-13 17:59:29 -0500

    Not working. I'll try again tomorrow, but for now I'm lost.

--- src/edu/yccc/cis174/mmuratake/Project1/File.java
+++ src/edu/yccc/cis174/mmuratake/Project1/File.java
@@ -6,6 +6,7 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.List;
 
 public class File {
 	
@@ -18,12 +19,7 @@ public class File {
             FileWriter fileWriter = new FileWriter(fileName);
             BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
 
-            bufferedWriter.write("Hello there,");
-            bufferedWriter.write(" here is some text.");
-            bufferedWriter.newLine();
-            bufferedWriter.write("We are writing");
-            bufferedWriter.write(" the text to the file.");
-
+           
             bufferedWriter.close();
         }
         catch(IOException ex) 
@@ -35,16 +31,24 @@ public class File {
 	public static void readFile() 
 	{
         String fileName = "JavaTestQuestions.txt";
-        String line = null;
-
         try 
         {
             FileReader fileReader = new FileReader(fileName);
             BufferedReader bufferedReader = new BufferedReader(fileReader);
 
-            while((line = bufferedReader.readLine()) != null) 
+            while((bufferedReader.readLine()) != null) 
             {
-            		System.out.println(line);
+            		JavaTest jv = new JavaTest();
+            		List<Questions> testQuestions = jv.loadQuestions();
+            		
+            		for(Questions questions2 : testQuestions)
+            		{
+            			if(!fileName.contains(questions2.getQuestion()))
+            			{
+            				//?????????
+            			}
+            		}
+
             }   
 
             bufferedReader.close();
