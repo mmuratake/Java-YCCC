1bfe00a341909d31695309b1614a82751ac0ab28
sMamiMuratake <sMamiMuratake@C218-1761.yccc.edu>
2018-02-14 18:29:43 -0500

    Added to notes, about Error Handling.

---
+++ src/edu/yccc/cis174/mmuratake/Notes/ErrorHandling.java
@@ -0,0 +1,25 @@
+package edu.yccc.cis174.mmuratake.Notes;
+
+public class ErrorHandling {
+	
+	//See Class ReadFile under package FileIO.
+	
+	
+	/**
+	 * Try Catch:
+	 * 
+	 * Let me try this; if it fails, then tell the user what went wrong.
+	 * 
+	 */
+	// Catch what you think might go wrong. (Ex: FileNotFoundException e)
+	// When you catch, ALWAYS have it print the stack trace. (e.printStackTrace();)
+	
+	/**
+	 * Throw:
+	 * 
+	 * Ehh, let me just shove my problems over to another person.
+	 * 
+	 */
+	// But SOMEONE has to catch it. Preferably the one closest to the user.
+
+}
