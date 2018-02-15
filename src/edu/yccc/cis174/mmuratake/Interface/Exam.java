4246d59bc185e5d33e8f6675302995b74d43c195
sMamiMuratake <sMamiMuratake@C218-1761.yccc.edu>
2018-02-14 19:57:00 -0500

    Lesson on interface. Easy! (The concept is.)

---
+++ src/edu/yccc/cis174/mmuratake/Interface/Exam.java
@@ -0,0 +1,13 @@
+package edu.yccc.cis174.mmuratake.Interface;
+
+public interface Exam {
+	
+	// Don't implement code, just define interface!
+	
+	public void askQuestions();
+	
+	public void getAllottedTime();
+	
+	public int getScore();
+
+}
---
+++ src/edu/yccc/cis174/mmuratake/Interface/Technology.java
@@ -0,0 +1,30 @@
+package edu.yccc.cis174.mmuratake.Interface;
+
+public class Technology implements Exam {
+	
+	// These down below are automatically generated when you fix the error!
+	// You NEED these to be an implementation of Exam.
+
+	
+	@Override
+	public void askQuestions() 
+	{
+		// TODO Auto-generated method stub
+	}
+
+	@Override
+	public void getAllottedTime() 
+	{
+		// TODO Auto-generated method stub
+	}
+
+	@Override
+	public int getScore() 
+	{
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	
+	
+
+}
