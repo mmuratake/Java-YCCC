5f7622a2d616500c56690f71f9af4da181dc4df8
sMamiMuratake <sMamiMuratake@C218-1761.yccc.edu>
2018-02-14 19:21:05 -0500

    Starting lesson on inheritance.

---
+++ src/edu/yccc/cis174/mmuratake/Inheritance/App.java
@@ -0,0 +1,14 @@
+package edu.yccc.cis174.mmuratake.Inheritance;
+
+public class App {
+	
+	public static void main(String[] args)
+	{
+		Question q = new Question();
+		q.setQuestion("Who invented Java?");
+		q.setAnswer("James Gossling");
+		
+		System.out.println(q);
+	}
+
+}
---
+++ src/edu/yccc/cis174/mmuratake/Inheritance/Question.java
@@ -0,0 +1,33 @@
+package edu.yccc.cis174.mmuratake.Inheritance;
+
+public class Question {
+	
+	String question;
+	String answer;
+	
+	public String getQuestion() 
+	{
+		return question;
+	}
+
+	public void setQuestion(String question) 
+	{
+		this.question = question;
+	}
+
+	public String getAnswer()
+	{
+		return answer;
+	}
+
+	public void setAnswer(String answer) 
+	{
+		this.answer = answer;
+	}
+
+	public String toString()
+	{
+		return "";
+	}
+
+}
