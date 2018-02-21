ca831db8824a30d3a3e93b2a9613b1a5439af697
sMamiMuratake <sMamiMuratake@C218-1761.yccc.edu>
2018-02-21 13:45:22 -0500

    Trying to read from file, with better motivation this time.

---
+++ src/edu/yccc/cis174/mmuratake/Project1/CreateQuestions.java
@@ -0,0 +1,19 @@
+package edu.yccc.cis174.mmuratake.Project1;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CreateQuestions {
+	
+	public static List<Questions> loadQuestions()
+	{
+		List<Questions> questions = new ArrayList<Questions>();
+		
+		
+
+		
+		return questions;
+		
+	}
+
+}
