dfc9c5748539a6b98db9697814a069af03c3c4e0
sMamiMuratake <sMamiMuratake@C218-1761.yccc.edu>
2018-02-21 14:39:48 -0500

    Vince might have just saved my life. Let's find out.

--- src/edu/yccc/cis174/mmuratake/Project1/CreateQuestions.java
+++ src/edu/yccc/cis174/mmuratake/Project1/CreateQuestions.java
@@ -2,6 +2,7 @@ package edu.yccc.cis174.mmuratake.Project1;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class CreateQuestions {
 	
@@ -9,11 +10,23 @@ public class CreateQuestions {
 	{
 		List<Questions> questions = new ArrayList<Questions>();
 		
+		String fileName = "JavaTestQuestions";
+		String fileName2 = "JavaTestAnswers";
 		
-
+		Scanner fileScanner = new Scanner(fileName);
+		Scanner fileScanner2 = new Scanner(fileName2);
+		
+		while(fileScanner.hasNextLine() && fileScanner2.hasNextLine()) 
+		{
+			String question = fileScanner.nextLine();
+			String answer = fileScanner.nextLine();
+			Questions q = new Questions(question, answer);
+			questions.add(q);
+		}
+		
+		fileScanner.close();
+		fileScanner2.close();
 		
 		return questions;
-		
 	}
-
 }
