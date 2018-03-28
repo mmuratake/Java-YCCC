0a9db742d387a02f08ef164712368a4bcb68352a
sMamiMuratake <sMamiMuratake@C218-1761.yccc.edu>
2018-03-28 17:56:55 -0400

    Started lesson on recursion.

---
+++ src/edu/yccc/cis174/mmuratake/Recursion/Recursive.java
@@ -0,0 +1,16 @@
+package edu.yccc.cis174.mmuratake.Recursion;
+
+public class Recursive {
+	
+	public void walkDirectories(String filepath)
+	{
+		// 1. List files.
+		// 2. If a directory exists, call self, pass the directory path.
+	}
+	
+	public static void main(String[] args)
+	{
+		
+	}
+
+}
