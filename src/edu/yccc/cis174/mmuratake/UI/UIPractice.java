6d481ff29ee6e107caaf500e8bcd997b23f09ea6
sMamiMuratake <sMamiMuratake@C218-1761.yccc.edu>
2018-04-04 17:37:53 -0400

    Let's see if I ran into a Gittastrophy or not.

---
+++ src/edu/yccc/cis174/mmuratake/UI/UIPractice.java
@@ -0,0 +1,51 @@
+package edu.yccc.cis174.mmuratake.UI;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class UIPractice {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) 
+	{
+		EventQueue.invokeLater(new Runnable() 
+		{
+			public void run() 
+			{
+				try 
+				{
+					UIPractice window = new UIPractice();
+					window.frame.setVisible(true);
+				} 
+				catch (Exception e) 
+				{
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public UIPractice() 
+	{
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() 
+	{
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
