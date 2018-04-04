7f409645d953896e6a8b424aa97d6b6dde9a6dc5
sMamiMuratake <sMamiMuratake@C218-1761.yccc.edu>
2018-04-04 18:15:21 -0400

    Added notes on what the heck REST is. Time to project project again.

---
+++ src/edu/yccc/cis174/mmuratake/Notes/REST.java
@@ -0,0 +1,28 @@
+package edu.yccc.cis174.mmuratake.Notes;
+
+// April 4 2018. Happy Birthday Lest!
+
+public class REST {
+	
+	
+	// Rest is
+	// 	-HTTP
+	//		- Stateless
+	//			- Hey, I'm Mike! x 24 (Never remembers being asked, never gets bothered.)
+	//		- Post parameters
+	//		- Answers in JSon
+	
+	// [ You create this. It's a new library. ] 
+	
+	
+	/**
+	 * 
+	 * mySlack s = new mySlack();
+	 * s.callSlack("txt");
+	 * 
+	 * 
+	 *  [ Someone else did the work, you consume it. It's an external library. ]
+	 * 
+	 */
+
+}
