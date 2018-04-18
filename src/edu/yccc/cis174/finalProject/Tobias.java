a911eb1767156a9a0d10e4508a801a86994cea69
sMamiMuratake <sMamiMuratake@C218-1761.yccc.edu>
2018-04-18 13:35:21 -0400

    Trying to move along with the final project. Ideas are getting clearer.

--- src/edu/yccc/cis174/finalProject/Tobias.java
+++ src/edu/yccc/cis174/finalProject/Tobias.java
@@ -1,7 +1,57 @@
 package edu.yccc.cis174.finalProject;
 
-public class Tobias {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class Tobias implements Character{
 	
 	// Each character has a list of stats. One out of the list is chosen to determine which one is "set" for the round.
+	
+	private static Random random = new Random();
 
+	public String name() {
+		return "Tobias";
+	}
+
+	public void getLikes() {
+		List<String> likes = new ArrayList<String>();
+		likes.add("hot dogs");
+		likes.add("chocolates");
+		likes.add("cool watches");
+		likes.add("Coke");
+		int index = random.nextInt(likes.size());
+		System.out.println(likes.get(index));
+	}
+
+	@Override
+	public void getDislikes() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void getHobbies() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void getFriends() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void getFamily() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void greeting() 
+	{
+		System.out.println("Hey. You play before?");
+		
+	}
+	
 }
