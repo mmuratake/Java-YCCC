4052823ded71be54dde97211e8c367fd7112e0a7
sMamiMuratake <sMamiMuratake@Titanic-1436.yccc.edu>
2018-05-02 15:25:59 -0400

    Moved methods around, cleared text file, added comments.

--- src/edu/yccc/cis174/mmuratake/finalproject/Character.java
+++ src/edu/yccc/cis174/mmuratake/finalproject/Character.java
@@ -3,13 +3,12 @@ package edu.yccc.cis174.mmuratake.finalproject;
 public interface Character {
 	
 	public String name();
+	public void greeting();
 	public void likes();
 	public void dislikes();
 	public void hobbies();
 	public void friends();
 	public void family();
-	
-	public void greeting();
 	public void guess();
 
 }
